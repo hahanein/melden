0257a877477d60567f0b4710ea629e3963c44949
hahanein <benjamin.westphal@riseup.net>
2023-12-03 20:05:33 +0100

    remove dependencies not required in bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import {resolve} from "node:path";
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/main.ts"),
@@ -12,7 +11,7 @@ export default defineConfig({
 			fileName: "melden",
 		},
 		rollupOptions: {
-			external: ["react", "react-dom", "framer-motion"],
+			external: ["react", "react/jsx-runtime", "framer-motion"],
 		},
 	},
 });
